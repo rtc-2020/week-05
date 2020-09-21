7a95118f6ff333eafa498703caecf49821afa04f
Karl Stolley <karl.stolley@gmail.com>
2020-09-21 14:48:54 -0500

    Listen for 'Do work' and then do work

--- worker.js
+++ worker.js
@@ -4,4 +4,18 @@ onmessage = function(data) {
   if (data.data == 'Can you hear me, Worker?') {
     postMessage('Yes, I can hear you!');
   }
+  if (data.data == 'Do work') {
+    (async function() {
+      var result = await doHardWork();
+      postMessage(`I have finished my work! ${result}`);
+    })();
+  }
 };
+
+function doHardWork() {
+  var i;
+  for (i=0; i < 100000000; i++) {
+    i = i * 2000;
+  }
+  return i;
+}
