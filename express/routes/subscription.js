6688cb7c9c4268407359b7e3573fbb9b631cdf67
Karl Stolley <karl.stolley@gmail.com>
2020-09-24 14:01:16 -0500

    Handle line-delimited JSON in subscriptions

--- express/routes/subscription.js
+++ express/routes/subscription.js
@@ -5,9 +5,8 @@ const fs = require('fs');
 const router = express.Router();
 
 router.post('/', function(req, res, next) {
-  var data = JSON.stringify(req.body) + '\n';
-  console.log('From the new router...')
-  console.log(data);
+  var data = JSON.stringify(req.body) + '\n'; // newline to create NDJSON
+  // console.log(data);
   fs.promises.appendFile('var/subscriptions.json', data, {encoding:'utf8'})
   .then(function(result) {
     res.json({status: 'subscribed'});
